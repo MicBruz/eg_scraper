2682f829ad403f677c6691fd4c3d3aa6021c6e6c
Michal Bru <mbruzda1@wp.pl>
2022-01-23 16:20:18 +0100

    pull structured data from one page on a click and render json

---
+++ app/controllers/scrapers_controller.rb
@@ -0,0 +1,60 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+class ScrapersController < ApplicationController
+  def home; end
+
+  def get_data
+
+    url = URI('https://www.e-glamour.pl/services/stargate/product-list?c=613&f%5C%5Bsex%5C%5D%5C%5B%5C%5D=4&f%5C%5Bsex%5C%5D%5C%5B%5C%5D=5&page=3')
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request['authority'] = 'www.e-glamour.pl'
+    request['sec-ch-ua'] = '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"'
+    request['content-type'] = 'application/json'
+    request['identity'] = '7bd163ade5decb7d34466b8b4cf9f0fc'
+    request['sec-ch-ua-mobile'] = '?0'
+    request['user-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
+    request['sec-ch-ua-platform'] = '"macOS"'
+    request['instance'] = 'egpl'
+    request['accept'] = '*/*'
+    request['sec-fetch-site'] = 'same-origin'
+    request['sec-fetch-mode'] = 'cors'
+    request['sec-fetch-dest'] = 'empty'
+    request['referer'] = 'https://www.e-glamour.pl/perfumy/?c=613&f[sex][]=4&page=2&f[sex][]=5'
+    request['accept-language'] = 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7'
+    request['cookie'] = 'visit_start=%7B%22referer%22:%22%22,%22time%22:1642865198278,%22url%22:%22https://www.e-glamour.pl/perfumy/?c=613&f%5Bsex%5D%5B%5D=4&page=2&f%5Bsex%5D%5B%5D=5%22%7D; identity=7bd163ade5decb7d34466b8b4cf9f0fc; PHPSESSID=7bd163ade5decb7d34466b8b4cf9f0fc; _gcl_au=1.1.1454671233.1642865200; percent_sale=0; _ga=GA1.2.1335135212.1642865200; _gid=GA1.2.933556858.1642865200; _dc_gtm_UA-6319024-1=1'
+
+    response = http.request(request)
+    response = JSON.parse(response.body)
+    total_count = { total_count: response['total_count'] }
+    data = { items: response['items'] }
+
+    items_sorted = {}
+
+    # n+1 issue
+
+    data[:items].each do |item|
+      name_heureka = item['name_heureka']
+      variants = []
+      item['variants_in_stock']&.each do |var|
+        variants << { variant_name: var['variantName'],
+                      variant_price: var['price'],
+                      url: (request['authority'] + var['curl']).to_s }
+      end
+      items_sorted[name_heureka] = variants
+    end
+
+
+    render json: items_sorted
+  end
+
+  # render json: data[:items][0]["variants_in_stock"][0]["price"]
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'scrapers/home'
+  get '/scrapers/get_data', to: 'scrapers#get_data'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
