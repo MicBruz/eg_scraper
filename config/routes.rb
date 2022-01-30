e65d777bdb847b7950fda3989bed2d44c43b868c
Michal Bru <mbruzda1@wp.pl>
2022-01-30 18:09:30 +0100

    create table view for fragrances data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'scrapers/home'
-  get '/scrapers/get_data', to: 'scrapers#get_data'
+  get 'scrapers/get_data'
+  # get '/scrapers/get_data', to: 'scrapers#get_data'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
