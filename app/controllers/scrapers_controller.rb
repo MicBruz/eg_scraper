5090cdf7c589f5356e31b6e557d6f2963194406c
Michal Bru <mbruzda1@wp.pl>
2022-04-09 19:36:35 +0200

    change layout, add difference column

--- app/controllers/scrapers_controller.rb
+++ app/controllers/scrapers_controller.rb
@@ -8,7 +8,10 @@ class ScrapersController < ApplicationController
   def home
     @number_of_mens_fragrances = FilteredBySex.last.male
     @number_of_unisex_fragrances = FilteredBySex.last.unisex
-    @last_pull = FilteredBySex.last.created_at if FilteredBySex.last.present?
+    @last_pull = FilteredBySex.last_pull
+    @previous_pull = FilteredBySex.previous_pull
+    @change_male = FilteredBySex.difference_to_previous_pull("male")
+    @change_unisex = FilteredBySex.difference_to_previous_pull("unisex")
   end
 
   def get_items
--- app/models/filtered_by_sex.rb
+++ app/models/filtered_by_sex.rb
@@ -1,2 +1,20 @@
 class FilteredBySex < ApplicationRecord
+
+  private
+
+  def self.last_pull
+    last_pull = last.created_at.localtime
+    last_pull_time = last_pull.strftime("%H:%M:%S")
+    last_pull_date = (last_pull.today? ? "Today" : last_pull.strftime("%d.%m.%y"))
+
+    { time: last_pull_time, date: last_pull_date }
+  end
+
+  def self.previous_pull
+    ((last.created_at - second_to_last.created_at) / 60).round
+  end
+
+  def self.difference_to_previous_pull(sex)
+    sex == "male" ? last.male - second_to_last.male : last.unisex - second_to_last.unisex
+  end
 end
